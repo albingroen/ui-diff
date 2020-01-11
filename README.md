af67984d6be99855806bcebf093c96aad0efd8a7
Albin Groen <albin.groen@gmail.com>
2020-01-11 17:36:46 +0100

    🎉 Init commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# ui-diff-script
+<center><img src="./static/readme.png" width="800" /></center>
+
+# ui-diff
+
+The tool for frontend screenshot testing
+
+## How to use it
+
+### 1. Create a new project on app.ui-diff.com
+
+### 2. Install dependency with yarn or npm
+
+    yarn add ui-diff
+
+### 3. Supply configuration files (in project root)
+
+These can be downloaded at app.ui-diff.com/documentation
+
+1. ui-diff-config.js
+2. ui-diff-pages.js
+
+### 4. Update configuration files
+
+ui-diff.config.js
+1. Add your environments
+2. Add project API-token
+
+ui-diff-pages.js
+1. Add the pages you want to test
+
+### 5. Run screenshot tests
+
+    yarn screenshots
+
+## Built using
+
+- 📸 [puppeteer](https://pptr.dev/)
+- 🌐 [ora](https://github.com/sindresorhus/ora)
+- ⚙️ [minimist](https://github.com/substack/minimist)
+- ⚡️ [axios](https://github.com/axios/axios)
+
+## Contributors
+
+[Albin Groen](https://github.com/albingroen)
---
+++ index.js
@@ -0,0 +1,126 @@
+#! /usr/bin/env node
+const puppeteer = require("puppeteer");
+const axios = require("axios");
+const ora = require("ora");
+const pages = require("../../ui-diff-pages");
+const config = require("../../ui-diff-config");
+
+// Parsed command line args
+const args = require("minimist")(process.argv.slice(2));
+
+// Find out if to launch headless or headful
+const headless =
+  !args.headless || args.headless === "true" || args.headless === true;
+
+// Main function
+async function main() {
+  process.setMaxListeners(Infinity);
+
+  // Launch headless/headful browser
+  const browser = await puppeteer.launch({
+    headless
+  });
+
+  // Initate progress bar
+  const spinner = ora(
+    `Spinning up head${headless ? "less" : "ful"} browser`
+  ).start();
+
+  // Keep track of amount of pages screenshotted
+  let progress = 0;
+
+  // Async function for taking a screenshot
+  async function run(url, name, variant) {
+    const page = await browser.newPage();
+
+    // Set browser dimensions for screenshot
+    const sizes = {
+      phone: {
+        width: 375,
+        height: 812
+      },
+      tablet: {
+        width: 768,
+        height: 1024
+      },
+      computer: {
+        width: 1920,
+        height: 1080
+      }
+    };
+
+    await page.setViewport(
+      args.size ? sizes[args.size] || sizes.computer : sizes.computer
+    );
+
+    // Go to page and if fails close browser
+    try {
+      await page.goto(url, { timeout: 0, waitUntil: "networkidle2" });
+    } catch (e) {
+      browser.close();
+    }
+
+    // Take screenshot and close page. On last page also close browser
+    try {
+      await page
+        .screenshot({
+          encoding: "binary",
+          fullPage: true,
+          type: "jpeg",
+          quality: 65
+        })
+        .then(imageBuffer => {
+          progress = progress + 1;
+          page.close().then(async () => {
+            // Update spinner
+            spinner.text = `Taking screenshots [${progress}/${pages.length *
+              (args.env ? 1 : 2)}]`;
+
+            // Upload image to ui-diff
+            await axios.post(
+              "https://infinite-plains-43173.herokuapp.com/projects/images",
+              { image: imageBuffer, env: variant, name },
+              { headers: { "api-token": config.token } }
+            );
+
+            // On last page close browser and stop spinner
+            if (progress === pages.length * (!args.env ? 2 : 1)) {
+              browser
+                .close()
+                .then(() => {
+                  spinner.stop();
+                  // eslint-disable-next-line no-console
+                  console.log("Results available on https://app.ui-diff.com/");
+                })
+                .catch(e => {
+                  // eslint-disable-next-line no-console
+                  console.error(e);
+                });
+            }
+          });
+        });
+    } catch (e) {
+      browser.close();
+    }
+  }
+
+  const envs = config.envs;
+
+  if (!args.env || !envs[args.env]) {
+    // Screenshot pages on localhost
+    pages.forEach(p => {
+      run(`${envs.local}${p.path}`, p.name, "local");
+    });
+
+    // Screenshot pages on live
+    pages.forEach(p => {
+      run(`${envs.live}${p.path}`, p.name, "live");
+    });
+  } else {
+    pages.forEach(p => {
+      run(`${envs[args.env]}${p.path}`, p.name, args.env);
+    });
+  }
+}
+
+main();
